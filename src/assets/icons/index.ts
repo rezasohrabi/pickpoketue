4e64f8423e3eaafd63817cb1e983ee922f4558ce
rezasohrabi <rezasohrabi1995@yahoo.com>
2022-06-09 23:45:34 +0430

    chore: add icons and images

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as PlusIcon } from './plusIcon.svg';
+export { default as CaretDownIcon } from './caretDown.svg';
+export { default as AnswerIcon } from './answerIcon.svg';
---
+++ src/assets/images/index.ts
@@ -0,0 +1,1 @@
+export { default as AvatarImage } from './avatar.png';
