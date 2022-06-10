480d7037714a3a8fe9fdaee93ba6407ab24be016
rezasohrabi <rezasohrabi1995@yahoo.com>
2022-06-10 22:06:01 +0430

    feat: add icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,3 +2,5 @@ export { default as PlusIcon } from './plusIcon.svg';
 export { default as CaretDownIcon } from './caretDown.svg';
 export { default as AnswerIcon } from './answerIcon.svg';
 export { default as CloseIcon } from './closeIcon.svg';
+export { default as LikeIcon } from './likeIcon.svg';
+export { default as DislikeIcon } from './disLikeIcon.svg';
