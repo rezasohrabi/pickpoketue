efa8c5f4b4920fab42b3558a4fb93c6bcf87c084
rezasohrabi <rezasohrabi1995@yahoo.com>
2022-06-11 10:24:39 +0430

    feat: fetchAll questions in QuestionList page

--- src/pages/QuestionListPage/QuestionListPage.tsx
+++ src/pages/QuestionListPage/QuestionListPage.tsx
@@ -1,7 +1,15 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect } from 'react';
 import { QuestionList, Header } from 'components';
+import { useAppDispatch } from 'store';
+import { fetchAllQuestions } from 'store/question/actions';
 
 export default function QuestionListPage(): ReactElement {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAllQuestions());
+  }, []);
+
   return (
     <>
       <Header title='لیست سوالات' />
