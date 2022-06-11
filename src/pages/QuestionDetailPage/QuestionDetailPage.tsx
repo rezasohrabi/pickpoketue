bb02c345719ed413dc1525c7e7c6d654bb18c88b
rezasohrabi <rezasohrabi1995@yahoo.com>
2022-06-11 10:23:37 +0430

    feat: handle fetch current question and answers by question id for QuestionDetail page

--- src/pages/QuestionDetailPage/QuestionDetailPage.tsx
+++ src/pages/QuestionDetailPage/QuestionDetailPage.tsx
@@ -3,28 +3,40 @@ import { Header } from 'components';
 import styled from 'styled-components';
 import { scrollToTop } from 'utils';
 import { AddAnswerForm, AnswerList, QuestionDetail } from 'components';
+import { useParams } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from 'store';
+import {
+  fetchAnswersByQuestionId,
+  fetchCurrentQuestion,
+} from 'store/question/actions';
+import { selectCurrentQuestion } from 'store/question/selectors';
 
 const Main = styled.main`
   margin: 32px 56px 60px;
 `;
 
 export default function QuestionDetailPage(): ReactElement {
+  const { questionId } = useParams();
+
+  const dispatch = useAppDispatch();
+  const currentQuestion = useAppSelector((state) =>
+    selectCurrentQuestion(state)
+  );
+
   useEffect(() => {
     scrollToTop();
+    dispatch(fetchCurrentQuestion(String(questionId)));
   }, []);
+
+  useEffect(() => {
+    dispatch(fetchAnswersByQuestionId(String(questionId)));
+  }, []);
+
   return (
     <>
       <Header title='جزییات سوال' />
       <Main>
-        <QuestionDetail
-          id='1'
-          questionerImage='https://randomuser.me/api/portraits/men/29.jpg'
-          title='مشکل در واث در ری اکت'
-          time='16:48'
-          date='15/2/1373'
-          answerCount={20}
-          text='لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.'
-        />
+        {currentQuestion && <QuestionDetail {...currentQuestion} />}
         <AnswerList />
         <AddAnswerForm />
       </Main>
