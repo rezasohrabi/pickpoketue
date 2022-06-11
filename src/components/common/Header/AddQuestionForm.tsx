d8c48a10b925081d198d291b1a2743a8d18a6cfc
rezasohrabi <rezasohrabi1995@yahoo.com>
2022-06-11 10:10:07 +0430

    refactor : create a question functionallity was implemented

--- src/components/common/Header/AddQuestionForm.tsx
+++ src/components/common/Header/AddQuestionForm.tsx
@@ -1,11 +1,18 @@
 import { ReactElement } from 'react';
 import { TextInput, TextArea } from 'components';
 
-export default function AddQuestionForm(): ReactElement {
+interface AddQuestionFormProps {
+  onTextChange: (e: React.FormEvent<HTMLInputElement>) => void;
+  onTextAreaChange: (e: React.FormEvent<HTMLTextAreaElement>) => void;
+}
+export default function AddQuestionForm({
+  onTextChange,
+  onTextAreaChange,
+}: AddQuestionFormProps): ReactElement {
   return (
     <>
-      <TextInput label='موضوع' />
-      <TextArea label='متن سوال' />
+      <TextInput label='موضوع' onChange={(e) => onTextChange(e)} />
+      <TextArea label='متن سوال' onChange={(e) => onTextAreaChange(e)} />
     </>
   );
 }
--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useRef } from 'react';
 import { CaretDownIcon, PlusIcon, AvatarImage } from 'assets';
 import {
   StyledHeader,
@@ -13,6 +13,9 @@ import {
 import { useModal } from 'hooks';
 import { Modal, Button } from 'components';
 import AddQuestionForm from './AddQuestionForm';
+import { useAppDispatch } from 'store';
+import { createQuestion } from 'store/question/actions';
+import { randomTo, uniqueId } from 'utils/random';
 
 interface HeaderProps {
   title: string;
@@ -20,12 +23,46 @@ interface HeaderProps {
 
 export default function Header({ title }: HeaderProps): ReactElement {
   const { isVisible, show, hide } = useModal();
+  const dispatch = useAppDispatch();
+
+  const questionText = useRef<string>('');
+  const questionTitle = useRef<string>('');
+
+  const handleCreateQuestion = () => {
+    if (!questionText.current || !questionTitle.current) {
+      throw new Error('Field is required');
+    }
+    dispatch(
+      createQuestion({
+        id: uniqueId(),
+        text: questionText.current,
+        answerCount: 0,
+        date: '1400/03/21',
+        questionerImage: `https://randomuser.me/api/portraits/${
+          ['men', 'women'][randomTo(1)]
+        }/${randomTo(100)}.jpg`,
+        time: '12:25',
+        title: questionTitle.current,
+      })
+    );
+    hide();
+  };
   return (
     <StyledHeader>
       <StyledH1>{title}</StyledH1>
       <HeaderLeftSide>
-        <Modal isVisible={isVisible} hide={hide} title='ایجاد سوال جدید'>
-          <AddQuestionForm />
+        <Modal
+          isVisible={isVisible}
+          hide={hide}
+          title='ایجاد سوال جدید'
+          onActionClick={handleCreateQuestion}
+        >
+          <AddQuestionForm
+            onTextAreaChange={(e: any) =>
+              (questionText.current = e.target.value)
+            }
+            onTextChange={(e: any) => (questionTitle.current = e.target.value)}
+          />
         </Modal>
         <Button onClick={show}>
           <StyledPlusIcon icon={PlusIcon} />
